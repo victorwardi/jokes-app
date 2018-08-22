ca764a6827673fe407f1787d5d2e29815db22877
Victor Wardi <victorcaetano@live.com>
2018-08-22 10:07:36 -0300

    Initial commit

---
+++ src/test/java/guru/springframework/jokesapp/service/JokesServiceTest.java
@@ -0,0 +1,12 @@
+package guru.springframework.jokesapp.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JokesServiceTest {
+
+    @Test
+    public void getJoke() {
+    }
+}
